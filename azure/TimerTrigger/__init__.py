4ed86284c1a79c0ba0f6343fc626592942104ef8
Konstantin Weddige <konstantin@skathi.net>
2022-12-18 20:36:07 +0100

    Remove clutter

--- azure/TimerTrigger/__init__.py
+++ azure/TimerTrigger/__init__.py
@@ -7,13 +7,6 @@ from tootify.tootifier import Tootifier
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info("The timer is past due!")
-
     tootifyer = Tootifier(Path("/tootify/config.yaml"))
     tootifyer.connect()
     tootifyer.toot_new_tweets()
-
-    logging.info("Python timer trigger function ran at %s", utc_timestamp)

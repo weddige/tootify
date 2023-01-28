910b353a19fe6c2e054067d617095a46045891ac
Konstantin Weddige <konstantin@skathi.net>
2023-01-28 21:23:41 +0100

    Fix trigger

--- azure/TimerTrigger/__init__.py
+++ azure/TimerTrigger/__init__.py
@@ -10,9 +10,12 @@ logger = logging.getLogger(__name__)
 
 
 def main(timer: func.TimerRequest) -> None:
-    config = os.environ.get("TOOTIFIER_CONFIG") or "/tootify/config.yaml"
-    for path in config.split(":"):
-        logger.info(f"Run tootifier {path}")
-        tootifyer = Tootifier(Path(path))
-        tootifyer.connect()
-        tootifyer.toot()
+    try:
+        config = os.environ.get("TOOTIFIER_CONFIG") or "/tootify/config.yaml"
+        for path in config.split(":"):
+            logger.info(f"Run tootifier {path}")
+            tootifyer = Tootifier(Path(path))
+            tootifyer.connect()
+            tootifyer.toot()
+    except Exception as e:
+        logger.critical(e)

8c77d28d862fe88321a728b3f1ab2bb9cfe24c20
Konstantin Weddige <konstantin.weddige@lutrasecurity.com>
2023-05-05 16:10:09 +0200

    Improve error handling

--- azure/TimerTrigger/__init__.py
+++ azure/TimerTrigger/__init__.py
@@ -10,12 +10,16 @@ logger = logging.getLogger(__name__)
 
 
 def main(timer: func.TimerRequest) -> None:
-    try:
-        config = os.environ.get("TOOTIFIER_CONFIG") or "/tootify/config.yaml"
-        for path in config.split(":"):
+    fails = 0
+    config = os.environ.get("TOOTIFIER_CONFIG") or "/tootify/config.yaml"
+    for path in config.split(":"):
+        try:
             logger.info(f"Run tootifier {path}")
             tootifyer = Tootifier(Path(path))
             tootifyer.connect()
             tootifyer.toot()
-    except Exception as e:
-        logger.critical(e)
+        except Exception as e:
+            logger.error(e)
+            fails += 1
+    if fails:
+        raise RuntimeError("{fails} executions failed")
